4248676524b2142f76062c208c3f0643e151e608
Michael Upjohn <michael.upjohn@hotmail.co.uk>
2020-04-18 23:58:27 +0100

    Flyout map

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -1,4 +1,19 @@
+var height = $(window).height();
+
 function loadStormChaseStats(e) {
     e.preventDefault();
     console.log("called load storm chase stats...");
+
+    $("#flyout-map").animate({"left":"0"}, "slow");
+	$("#flyout-map-area").height(height - 82);
+
+	window.removeEventListener('scroll', noScroll);
 }
+
+$("#flyout-map-close").click(function(e) {
+	e.preventDefault();
+
+	$("#flyout-map").animate({"left":"-1903"}, "slow");
+
+	window.addEventListener('scroll', noScroll);
+})
