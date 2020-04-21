465c328e0f22c84710aa8837c73cdb0d803c8d80
Michael Upjohn <michael.upjohn@hotmail.co.uk>
2020-04-21 22:22:52 +0100

    Need to split out drawing geojson line and markers.

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -38,74 +38,94 @@ function LoadMap() {
 
 		$("#flyout-map-area").data('loaded', true);
 
-		map.on('load', function() {
-			map.addSource('day-1', {
-				'type': 'geojson',
-				'data': {
-					'type': 'FeatureCollection',
-					'features': [
-						{
-							'type': 'Feature',
+		var featureCollection = {
+			'type': 'geojson',
+			'data': {
+				'type': 'FeatureCollection',
+				'features': [
+					{
+						'type': 'Feature',
 							'properties': {
-								'color':'#F7455D'
-							},
-							'geometry': {
-								'type':'LineString',
-								'coordinates':[
-									[-97.007589,32.836157],
-									[-97.347316,32.842589],
-									[-97.461385,32.786883],
-									[-97.483736,32.737301],
-									[-97.570058,32.720443],
-									[-97.696844,32.755452],
-									[-98.369694,32.512379],
-									[-98.830593,32.371882],
-									[-100.337265,32.488242],
-									[-101.442497,32.265628],
-									[-102.902266,31.566934],
-									[-102.848314,31.332861],
-									[-102.888393,30.897362]
-								]
-							}
+							'color':'#F7455D'
 						},
-						{
-							'type':'Feature',
-							'properties':{
-								'description':'<b>Cisco, TX</b><br/><p>Stopped here for lunch before the storm chase began.</p><a style="font-weight:bold" href="http://bbc.co.uk/">Click to access BBC</a>',
-								'icon':'theatre'
-							},
-							'geometry': {
-								'type': 'Point',
-								'coordinates': [-98.830593,32.371882]
-							}
+						'geometry': {
+							'type':'LineString',
+							'coordinates':[
+								[-97.007589,32.836157],
+								[-97.347316,32.842589],
+								[-97.461385,32.786883],
+								[-97.483736,32.737301],
+								[-97.570058,32.720443],
+								[-97.696844,32.755452],
+								[-98.369694,32.512379],
+								[-98.830593,32.371882],
+								[-100.337265,32.488242],
+								[-101.442497,32.265628],
+								[-102.902266,31.566934],
+								[-102.848314,31.332861],
+								[-102.888393,30.897362]
+							]
 						}
+					},
+					{
+						'type':'Feature',
+						'properties':{
+							'description':'<b>Cisco, TX</b><br/><p>Stopped here for lunch before the storm chase began.</p><a style="font-weight:bold" href="http://www.mike-upjohn.co.uk/">Click to access BBC</a>',
+							'icon':'rectangle-green'
+						},
+						'geometry': {
+							'type': 'Point',
+							'coordinates': [-98.830593,32.371882]
+						}
+					},
+					{
+						'type':'Feature',
+						'properties':{
+							'description':'<b>Monahans, TX</b><br/><p>Saw a nice storm here</p>',
+							'icon':'rectangle-green'
+						},
+						'geometry': {
+							'type': 'Point',
+							'coordinates': [-98.830593,32.371882]
+						}
+					}
+				]
+				
+			}
+		};
 
-					]
-					
-				}
-			});
+		var layer = {
+			'id':'lines',
+			'type':'line',
+			'source':'day-1',
+			'paint': {
+				'line-width': 5,
+				'line-color': ['get', 'color']
+			},
+			'filter': ['==', '$type','LineString']
+		};
 
-			map.addLayer({
-				'id':'lines',
-				'type':'line',
-				'source':'day-1',
-				'paint': {
-					'line-width': 5,
-					'line-color': ['get', 'color']
-				},
-				'filter': ['==', '$type','LineString']
-			});
+		var stops = {
+			'id':'day-1-stops',
+			'type':'symbol',
+			'source':'day-1',
+			'layout': {
+				'icon-image': '{icon}-3',
+				'icon-allow-overlap': true
+			},
+			'filter':['==','$type','Point']
+		};
 
-			map.addLayer({
-				'id':'day-1-stops',
-				'type':'symbol',
-				'source':'day-1',
-				'layout': {
-					'icon-image': '{icon}-15',
-					'icon-allow-overlap': true
-				},
-				'filter':['==','$type','Point']
-			});
+		map.on('load', function() {
+			map.addSource('day-1', featureCollection);
+			map.addLayer(layer);
+			/*map.addLayer(stops);*/
+			featureCollection.data.features.forEach(function(marker) {
+				var element = document.createElement('div');
+				element.className = 'marker';
+
+				new mapboxgl.Marker(element).setLngLat(marker.geometry.coordinates).addTo(map);
+			})
 
 			map.on('click', 'day-1-stops', function(e) {
 				var coordinates = e.features[0].geometry.coordinates.slice();
