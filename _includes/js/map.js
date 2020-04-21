fee83933ea188da2c93d5f0f451575198048c2ec
Michael Upjohn <michael.upjohn@hotmail.co.uk>
2020-04-21 21:40:06 +0100

    Icons on map route.

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -39,45 +39,94 @@ function LoadMap() {
 		$("#flyout-map-area").data('loaded', true);
 
 		map.on('load', function() {
-			map.addSource('route', {
+			map.addSource('day-1', {
 				'type': 'geojson',
 				'data': {
-					'type': 'Feature',
-					'properties': {
-						'color':'#F7455D'
-					},
-					'geometry': {
-						'type':'LineString',
-						'coordinates':[
-							[-97.007589,32.836157],
-							[-97.347316,32.842589],
-							[-97.461385,32.786883],
-							[-97.483736,32.737301],
-							[-97.570058,32.720443],
-							[-97.696844,32.755452],
-							[-98.369694,32.512379],
-							[-98.830593,32.371882],
-							[-100.337265,32.488242],
-							[-101.442497,32.265628],
-							[-102.902266,31.566934],
-							[-102.848314,31.332861],
-							[-102.888393,30.897362]
-						]
-					}
+					'type': 'FeatureCollection',
+					'features': [
+						{
+							'type': 'Feature',
+							'properties': {
+								'color':'#F7455D'
+							},
+							'geometry': {
+								'type':'LineString',
+								'coordinates':[
+									[-97.007589,32.836157],
+									[-97.347316,32.842589],
+									[-97.461385,32.786883],
+									[-97.483736,32.737301],
+									[-97.570058,32.720443],
+									[-97.696844,32.755452],
+									[-98.369694,32.512379],
+									[-98.830593,32.371882],
+									[-100.337265,32.488242],
+									[-101.442497,32.265628],
+									[-102.902266,31.566934],
+									[-102.848314,31.332861],
+									[-102.888393,30.897362]
+								]
+							}
+						},
+						{
+							'type':'Feature',
+							'properties':{
+								'description':'<b>Cisco, TX</b><br/><p>Stopped here for lunch before the storm chase began.</p><a style="font-weight:bold" href="http://bbc.co.uk/">Click to access BBC</a>',
+								'icon':'theatre'
+							},
+							'geometry': {
+								'type': 'Point',
+								'coordinates': [-98.830593,32.371882]
+							}
+						}
+
+					]
+					
 				}
 			});
 
 			map.addLayer({
 				'id':'lines',
 				'type':'line',
-				'source':'route',
+				'source':'day-1',
 				'paint': {
 					'line-width': 5,
 					'line-color': ['get', 'color']
-				}
+				},
+				'filter': ['==', '$type','LineString']
 			});
 
-			map.addSource('route2', {
+			map.addLayer({
+				'id':'day-1-stops',
+				'type':'symbol',
+				'source':'day-1',
+				'layout': {
+					'icon-image': '{icon}-15',
+					'icon-allow-overlap': true
+				},
+				'filter':['==','$type','Point']
+			});
+
+			map.on('click', 'day-1-stops', function(e) {
+				var coordinates = e.features[0].geometry.coordinates.slice();
+				var description = e.features[0].properties.description;
+
+				while(Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+					coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+				}
+
+				new mapboxgl.Popup().setLngLat(coordinates).setHTML(description).addTo(map);
+			});
+
+			map.on('mouseenter', 'day-1-stops', function() {
+				map.getCanvas().style.cursor = 'pointer';
+			});
+
+			map.on('mouseleave', 'day-1-stops', function() {
+				map.getCanvas().style.cursor = '';
+			})
+
+			/*map.addSource('route2', {
 				'type': 'geojson',
 				'data': {
 					'type': 'Feature',
@@ -115,7 +164,7 @@ function LoadMap() {
 					'line-width': 5,
 					'line-color': ['get', 'color']
 				}
-			});
+			});*/
 		});
 	}
 }
