54f0f516d2e915b6b2bceda30027e3b5586afad2
Michael Upjohn <michael.upjohn@hotmail.co.uk>
2020-05-25 20:00:27 +0100

    Working on gallery

--- _includes/js/gallery.js
+++ _includes/js/gallery.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 		$(this).css({height: galleryItemHeight + 'px'});
 		galleryTitle.css({ height: galleryItemHeight + 'px', bottom: (galleryItemHeight + 1) + 'px'});
 
-		$(this).find(".gallery-title").fadeIn();
+		$(this).find(".gallery-title").fadeIn(250);
 	});
 
 	$(".gallery-item").mouseleave(function(e) {
-		$(this).find(".gallery-title").fadeOut();
+		$(this).find(".gallery-title").fadeOut(250);
 	});
 });
