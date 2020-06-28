a487f4a0e7c97fd76535721c6c8bad3618363a02
MikeUpjohn <michael.upjohn@hotmail.co.uk>
2020-06-28 09:29:54 +0100

    Tweaking gallery.

--- _includes/js/gallery-details.js
+++ _includes/js/gallery-details.js
@@ -2,20 +2,24 @@ $(".gallery-image").click(function(e) {
 	e.preventDefault();
 	$.when($("#lightbox").fadeIn(750)).done(function() {
 		$("#lightbox").data('display', true);
-		$("#lightbox img").fadeIn(250);
+		$("#lightbox .image-holder img").fadeIn('slow');
+		$("#lightbox .caption").fadeIn('slow');
 	});
 });
 
 $("#gallery-image-close").click(function(e) {
 	e.preventDefault();
 	$("#lightbox").fadeOut(750);
-	$("#lightbox img").hide();
+	$("#lightbox .image-holder img").hide();
+	$("#lightbox .caption").hide();
 	$("#lightbox").data('display', false);
 });
 
 $(".lightbox").click(function(e) {
 	if($(this).data('display') == true) {
 		$(".lightbox").fadeOut(750);
+		$("#lightbox .image-holder img").hide();
+		$("#lightbox .caption").hide();
 		$("#lightbox").data('display', false);
 	}
 })
