31107284d2541d6cf3e75acdf370d6944fe8c803
MikeUpjohn <michael.upjohn@hotmail.co.uk>
2020-06-28 09:55:30 +0100

    Gallery tweaking.

--- _includes/js/gallery-details.js
+++ _includes/js/gallery-details.js
@@ -1,25 +1,34 @@
 $(".gallery-image").click(function(e) {
 	e.preventDefault();
-	$.when($("#lightbox").fadeIn(750)).done(function() {
-		$("#lightbox").data('display', true);
-		$("#lightbox .image-holder img").fadeIn('slow');
-		$("#lightbox .caption").fadeIn('slow');
-	});
+	fadeImageIn();
 });
 
 $("#gallery-image-close").click(function(e) {
 	e.preventDefault();
-	$("#lightbox").fadeOut(750);
-	$("#lightbox .image-holder img").hide();
-	$("#lightbox .caption").hide();
-	$("#lightbox").data('display', false);
+	fadeImageOut();
 });
 
 $(".lightbox").click(function(e) {
 	if($(this).data('display') == true) {
-		$(".lightbox").fadeOut(750);
+		fadeImageOut();
+	}
+});
+
+function fadeImageIn() {
+	$.when($("#lightbox").fadeIn(750)).done(function() {
+		$("#lightbox").data('display', true);
+		$.when($("#lightbox .image-holder img").fadeIn('slow')).done(function() {
+			$("#lightbox .image-holder").fadeIn('slow');
+			$("#lightbox .caption").fadeIn('slow');	
+		});
+	});
+}
+
+function fadeImageOut() {
+	$("#lightbox").fadeOut(750);
+	$.when($("#lightbox .image-holder").fadeOut('slow')).done(function(){
 		$("#lightbox .image-holder img").hide();
 		$("#lightbox .caption").hide();
 		$("#lightbox").data('display', false);
-	}
-})
+	});
+}
