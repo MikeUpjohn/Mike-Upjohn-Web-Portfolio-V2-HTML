af50bb36ddcd29ccd874eb2c1f5af3a9e7462ba0
MikeUpjohn <michael.upjohn@hotmail.co.uk>
2020-07-16 22:27:42 +0100

    Making the gallery awesome.

--- _includes/js/gallery-details.js
+++ _includes/js/gallery-details.js
@@ -1,44 +1,80 @@
-$(".gallery-image").click(function(e) {
+var imageUrl;
+var caption;
+var itemNumber;
+var last;
+var next;
+var totalItems;
+
+$(".gallery-item").click(function(e) {
 	e.preventDefault();
-	fadeImageIn();
+
+	imageUrl = $(this).data('imageurl');
+	caption = $(this).data('caption');
+	itemNumber = $(this).data('item');
+	totalItems = $("#gallery-item").length;
+
+	if(itemNumber === 0) {
+		last = 0;
+		next = itemNumber++;
+	}
+	else if(itemNumber === totalItems) {
+		last = itemNumber--;
+		next = 0;
+	}
+
+	fadeLightboxIn(imageUrl, caption);
 });
 
 $("#gallery-image-close").click(function(e) {
 	e.preventDefault();
-	fadeImageOut();
+	fadeLightboxOut();
 });
 
 $(".lightbox").click(function(e) {
 	if($(this).data('display') == true) {
-		fadeImageOut();
+		fadeLightboxOut();
 	}
 });
 
 $("#previous-gallery-item").click(function(e) {
 	e.stopPropagation();
+	e.preventDefault();
+
+	// get last image data
 	console.log("go left");
+
 });
 
 $("#next-gallery-item").click(function(e) {
 	e.stopPropagation();
+	e.preventDefault();
+
+	// get next image data
 	console.log("go right");
 });
 
-function fadeImageIn() {
-	$.when($("#lightbox").fadeIn(750)).done(function() {
-		$("#lightbox").data('display', true);
-		$.when($("#lightbox .image-holder img").fadeIn('slow')).done(function() {
-			$("#lightbox .image-holder").fadeIn('slow');
-			$("#lightbox .caption").fadeIn('slow');	
-		});
-	});
+function fadeLightboxIn(imageUrl, caption) {
+	changeGalleryImage(imageUrl, caption);
+	loadLightbox();
 }
 
-function fadeImageOut() {
-	$("#lightbox").fadeOut(750);
-	$.when($("#lightbox .image-holder").fadeOut('slow')).done(function(){
+function fadeLightboxOut() {
+	$.when($("#lightbox").fadeOut(750)).done(function() {
+		$("#lightbox .image-holder").hide();
 		$("#lightbox .image-holder img").hide();
 		$("#lightbox .caption").hide();
 		$("#lightbox").data('display', false);
 	});
+}
+
+function changeGalleryImage(imageUrl, caption) {
+	$("#lightbox .image-holder img").attr('src', imageUrl);
+	$("#lightbox .caption span").text(caption);
+}
+
+function loadLightbox() {
+	$.when($("#lightbox .image-holder").add($("#lightbox .image-holder img")).add($("#lightbox .caption")).fadeIn(750)).done(function() {
+		$("#lightbox").fadeIn(750);
+		$("#lightbox").data('display', true);
+	});		
 }
