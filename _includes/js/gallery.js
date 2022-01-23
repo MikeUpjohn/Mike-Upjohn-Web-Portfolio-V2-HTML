414196323e550d06f42037f46647164ba0478548
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2022-01-23 07:59:55 +0000

    #2 Mobile view of gallery tiles working.

--- _includes/js/gallery.js
+++ _includes/js/gallery.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
 	$(".gallery-item").mouseleave(function(e) {
 		$(this).find(".gallery-title").fadeOut(250);
 	});
+
+	setupGalleryTiles();
 });
+
+function setupGalleryTiles() {
+	if(window.width < 991) {
+		console.log("mobile view");
+
+		$(".gallery-item").each(function() {
+			var itemHeight = $(this).height();
+			$(this).css({height: itemHeight + 'px'});
+			$(this).find(".gallery-title").css({height: itemHeight + 'px', bottom: itemHeight + 'px'}).show();
+		});
+	}
+}
